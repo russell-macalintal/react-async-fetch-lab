a518965a453f31b5a7ac50d7467b8e45d141dfd9
russell-macalintal <russell-macalintal@gmail.com>
2021-08-24 05:33:26 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -18,8 +18,24 @@ export default class App extends React.Component {
 
 
     render() {
+        let names = "";
+        console.log(this.state.data);
+        if(this.state.data !== ""){
+            names = this.state.data.people.map((person, idx) => {
+                return <li>{person.name}</li>
+            })
+            console.log(names);
+        }
+                
         return (
-            <div>{this.state.data}</div>
+            <div>
+                <h2>
+                    People
+                </h2>
+                <div>
+                    {this.state.data !== "" ? names : null }
+                </div>
+            </div>
         )
     }
 }
